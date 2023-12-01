d093810622e062dfa7afc8dd0d4109ae26c9d511
AfonsoAbreu <65315866+AfonsoAbreu@users.noreply.github.com>
2023-12-01 00:00:16 -0300

    d

--- src/pages/Locais/styles.js
+++ src/pages/Locais/styles.js
@@ -1,13 +1,13 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 10,
-      alignItems: "center",
-    },
-    flatList: {
-      marginBottom: 10,
-      width: "100%"
-    },
+  container: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+  },
+  flatList: {
+    marginBottom: 10,
+    width: "100%"
+  },
 });
